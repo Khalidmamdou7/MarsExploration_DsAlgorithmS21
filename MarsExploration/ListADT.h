c7a66e94f5af2b5180630afa33427deb6b58ac37
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2021-06-03 05:12:15 +0200

    Removed a duplicate
    Removed a duplicate fn InsertBegin

--- MarsExploration/ListADT.h
+++ MarsExploration/ListADT.h
@@ -7,7 +7,6 @@ public:
 	virtual void ReadList()= 0;
 	virtual void InsertEnd(const T&)= 0;
 	virtual	void InsertBegin(const T&)= 0 ;
-	virtual void InsertBegin(const T&)=0;
 	virtual void DeleteAll()=0;
 	virtual bool DeleteNode(const T&)=0;
 	virtual T& traverselist_position(const T&)=0;
