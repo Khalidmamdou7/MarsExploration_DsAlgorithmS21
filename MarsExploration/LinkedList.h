b548de5b8112af91422c785d2376da707c0eb75e
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2021-06-03 05:13:12 +0200

    fixed a typo

--- MarsExploration/LinkedList.h
+++ MarsExploration/LinkedList.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ListADT.h"
-using namesapce std;
+using namespace std;
+
 template <typename T>
 class LinkedList : public listADT<T>
 {
@@ -37,129 +38,129 @@ LinkedList<T>::~LinkedList()
 
 template <typename T>
 void LinkedList<T>:: PrintList() const
+{
+	Node<T>* p = Head;
+	while (p)
 	{
-		Node<T>* p = Head;
-		while (p)
-		{
-			cout << p->getItem() << " ";
-			p = p->getNext();
-		}
-		cout << endl;
+		cout << p->getItem() << " ";
+		p = p->getNext();
+	}
+	cout << endl;
 }
 
 
 template <typename T>
 void LinkedList<T>::ReadList()	 
+{
+	int val;
+	cin >> val;
+	while (val != -1)
 	{
-		int val;
+		this->InsertEnd(val);
 		cin >> val;
-		while (val != -1)
-		{
-			this->InsertEnd(val);
-			cin >> val;
-		}
 	}
+}
 
 
 
 template <typename T>
 void LinkedList<T>::InsertEnd(const T& data)
+{
+	Node<T>* R = new Node<T>(data);
+	if (!Head)
 	{
-		Node<T>* R = new Node<T>(data);
-		if (!Head)
-		{
-			Head = R;
-			return;
-		}
-		Node<T>* p = Head;
-		while (p->getNext())
-			p = p->getNext();
-		p->setNext(R);
+		Head = R;
+		return;
 	}
+	Node<T>* p = Head;
+	while (p->getNext())
+		p = p->getNext();
+	p->setNext(R);
+}
 
 
 
 template <typename T>
 void LinkedList<T>::InsertBegin(const T& data)
+{
+	Node<T>* P = new Node<T>(data);
+	if (!Head)
 	{
-		Node<T>* P = new Node<T>(data);
-		if (!Head)
-		{
-			Head = P;
-			return;
-		}
-		P->setNext(Head);
 		Head = P;
+		return;
 	}
+	P->setNext(Head);
+	Head = P;
+}
 
 
 template <typename T>
 void LinkedList<T>::DeleteAll()
+{
+	Node<T>* P = Head;
+	while (Head)
 	{
-		Node<T>* P = Head;
-		while (Head)
-		{
-			P = Head->getNext();
-			delete Head;
-			Head = P;
-		}
+		P = Head->getNext();
+		delete Head;
+		Head = P;
 	}
+}
 
 
 
 template <typename T>
 bool LinkedList<T>::DeleteNode(const T& value)
-	 	{
-		Node<T>* P = Head;
-		Node<T>* temp = Head;
-		while (P)
+{
+	Node<T>* P = Head;
+	Node<T>* temp = Head;
+	while (P)
+	{
+		if (P->getItem() == value)
 		{
-			if (P->getItem() == value)
+			if (Head == NULL)
 			{
-				if (Head == NULL)
-				{
-					return NULL;
-				}
-				P = P->getNext();
+				return NULL;
 			}
-			delete temp;
+			P = P->getNext();
 		}
+		delete temp;
 	}
+}
 
 
 template <typename T>
 bool LinkedList<T>::traverselist_search(const T& value)
+{
+	int c = 0;
+	if (!Head)
+		return;
+	Node<T>* temp = Head;
+	while (!temp)
 	{
-		int c = 0;
-		if (!Head)
-			return;
-		Node<T>* temp = Head;
-		while (!temp)
-		{
-			c++;
-			if (temp->getItem() == value)
-				return c;
-			temp = temp->getNext();
-		}
-		return false
+		c++;
+		if (temp->getItem() == value)
+			return c;
+		temp = temp->getNext();
 	}
+	return false
+}
 
 
 template <typename T>
 T& LinkedList<T>::traverselist_position(const T& pos)
+{
+	int c = 0;
+	if (!Head)
+		return;
+	Node<T> t = Head;
+	while (!t)
 	{
-		int c = 0;
-		if (!Head)
-			return;
-		Node<T> t = Head;
-		while (!t)
+		c++;
+		if (c == pos)
 		{
-			c++;
-			if (c == pos)
-			{
-				return t->getItem();
-			}
+			return t->getItem();
 		}
 	}
+}
 
 
