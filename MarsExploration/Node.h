d7e9c0d4e5e2ef402c68e86efc5fe6a66bc1a72b
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2021-06-03 08:04:42 +0200

    Updated fns to use in PNode
    Made the setters and getters of next ptr virtual to override it on PNode

--- MarsExploration/Node.h
+++ MarsExploration/Node.h
@@ -10,9 +10,9 @@ public:
 	Node(const T& r_Item);
 	Node(const T& r_Item, Node<T>* nextNodePtr);
 	void setItem(const T& r_Item);
-	void setNext(Node<T>* nextNodePtr);
+	virtual void setNext(Node<T>* nextNodePtr);
 	T getItem() const;
-	Node<T>* getNext() const;
+	virtual Node<T>* getNext() const;
 }; // end Node
 
 
