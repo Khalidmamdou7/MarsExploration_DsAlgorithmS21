7cbe7ef204719f23bf784cd8b57b731bd3f2544d
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2021-06-04 10:42:57 +0200

    Added priority data member

--- MarsExploration/EmergencyMission.cpp
+++ MarsExploration/EmergencyMission.cpp
@@ -3,4 +3,18 @@
 EmergencyMission::EmergencyMission(int r_FD, int r_TargetLocation, int r_Duration, int r_Significance, char r_Status)
 	: Mission(r_FD, r_TargetLocation, r_Duration, r_Significance, r_Status)
 {
+	// Calculates Priority equation
+		// Priority = (Mission's significance*10) + Mission's Duration + Target Location - Formulation Day
+	priority = (r_Significance * 10) + r_Duration + r_TargetLocation - r_FD;
+
+}
+
+int EmergencyMission::getPriority() const
+{
+	return priority;
+}
+
+void EmergencyMission::setPriority(int p)
+{
+	priority = p;
 }
--- MarsExploration/EmergencyMission.h
+++ MarsExploration/EmergencyMission.h
@@ -3,9 +3,12 @@
 class EmergencyMission :
     public Mission
 {
-
+private:
+    int priority;
 public:
     EmergencyMission(int r_FD, int r_TargetLocation, int r_Duration, int r_Significance, char r_Status);
 
+    int getPriority() const;
+    void setPriority(int p);
 };
 
