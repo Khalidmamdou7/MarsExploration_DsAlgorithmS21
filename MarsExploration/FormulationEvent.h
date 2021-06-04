34f4ac0481e892951b43edcb744b8747e66a6e6c
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2021-06-04 10:40:50 +0200

    Implemented Execute function
    Not fully completed

--- MarsExploration/FormulationEvent.h
+++ MarsExploration/FormulationEvent.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "Event.h"
+
+#include "EmergencyMission.h"
+#include "PolarMissions.h"
+#include "MountainuousMission.h"
+
 class FormulationEvent :
     public Event
 {
@@ -8,6 +13,7 @@ private:
     int mTargetLocation;    // mission’s target location (in kilometers from the base station
     int mDuration;          // the number of days needed to fulfill the mission requirements at target location.
     int mSignificance;      // mission's significance
+    int mAutoP;
 public:
     FormulationEvent();
     FormulationEvent(int r_eventD, int r_mID, char r_mType, int r_mTargetLocation, int r_mDuration, int r_mSignificance);
@@ -35,4 +41,37 @@ inline FormulationEvent::FormulationEvent(int r_eventD, int r_mID, char r_mType,
 void FormulationEvent::Execute() {
     // TODO: Create a new mission and add it to the appropriate list
 
+    EmergencyMission* pEM = NULL;
+    PolarMissions* pPM = NULL;
+    MountainuousMission* pMM = NULL;
+    switch (mType) {
+    case 'E':
+        pEM = new EmergencyMission(FormulationDay, mTargetLocation, mDuration, mSignificance, 'W');
+        break;
+    case 'P':
+        pPM = new PolarMissions(FormulationDay, mTargetLocation, mDuration, mSignificance, 'W');
+        break;
+    case 'M':
+        pMM = new MountainuousMission(FormulationDay, mTargetLocation, mDuration, mSignificance, 'W', mAutoP);
+        break;
+    default:
+        break;
+    }
+
+    
+
+    if (pEM) {
+        // Add to priority queue of waiting Emergency mission using MarsStation
+        // pStation->get(pMission);
+    }
+    if (pPM) {
+        // Add to queue of waiting Polar missions using MarStation
+        // pStation->AddMission(pMission)
+    }
+    if (pMM) {
+        // Add to queue of waiting Mountainuous missions using MarStation
+
+    }
+
 }
+
