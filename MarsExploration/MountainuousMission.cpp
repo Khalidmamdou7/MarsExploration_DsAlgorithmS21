8b5896940d7a490d84560d3e5b4ca69573bfa498
Khalidmamdou7 <37771185+Khalidmamdou7@users.noreply.github.com>
2021-06-04 10:42:36 +0200

    Added a constructor

--- MarsExploration/MountainuousMission.cpp
+++ MarsExploration/MountainuousMission.cpp
@@ -5,6 +5,12 @@ MountainuousMission::MountainuousMission()
 	AutoP = -1;
 }
 
+MountainuousMission::MountainuousMission(int r_FD, int r_TargetLocation, int r_Duration, int r_Significance, char r_Status, int r_AutoP)
+	: Mission(r_FD, r_TargetLocation, r_Duration, r_Significance, r_Status)
+{
+	AutoP = r_AutoP;
+}
+
 int MountainuousMission::getAutoP() const
 {
 	return AutoP;
